f8287634965ce1e00a6178b3d972349c5dbc2ab3
chiefanunzio <chiefa.nunzio@gmail.com>
2021-03-22 19:04:00 +0100

    funziona peró?

--- js/main.js
+++ js/main.js
@@ -1,1 +1,27 @@
 
+function clickedNext(){
+    var startImg = $('.show-img');
+    var imgNext = startImg.next(); 
+    
+    startImg.removeClass('show-img');
+    imgNext.addClass('show-img');
+    
+}
+
+function clickedPrev(){
+    var startImg = $('.show-img');
+    var imgNext = startImg.prev(); 
+    
+    startImg.removeClass('show-img');
+    imgNext.addClass('show-img');
+    
+}
+
+
+function init(){
+    
+    $('.skip-next').click(clickedNext);
+    $('.skip-prev').click(clickedPrev);
+}
+
+$(document).ready(init)
